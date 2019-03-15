6751633648407f1baf82d3d3726b4b82929f4a3e
Csillag61 <csillagok@gmail.com>
2019-03-15 11:54:19 -0400

    updated login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -27,7 +27,7 @@ class Login extends React.Component {
   };
 
   handleSubmit = event => {
-    //event.preventDefault();
+    event.preventDefault();
     if (this.isFormValid(this.state)) {
       this.setState({ errors: [], loading: true });
       firebase
@@ -62,7 +62,7 @@ class Login extends React.Component {
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as="h1" icon color="violet" textAlign="center">
             <Icon name="code branch" color="violet" />
-            Login to Chat
+            Login to DevChat
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
