c9d6d5b1b043194d396b74a48a634b7891f935cb
Csillag61 <csillagok@gmail.com>
2019-03-15 10:19:01 -0400

    set up

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+
+var config = {
+    apiKey: "AIzaSyBiOAQ78VO9OY5QsugbyIGTYldi7bSEjoo",
+    authDomain: "react-slack-clone-76c14.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-76c14.firebaseio.com",
+    projectId: "react-slack-clone-76c14",
+    storageBucket: "react-slack-clone-76c14.appspot.com",
+    messagingSenderId: "596333842258"
+  };
+  firebase.initializeApp(config);
+
+
+  export default firebase;
